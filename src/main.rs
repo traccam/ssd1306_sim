4d41ed2de86e234850b14b302aa6f35ec182fb65
FlareFlo <mail@flareflo.dev>
2026-02-23 14:13:41 +0100

    Use traccam common

--- src/main.rs
+++ src/main.rs
@@ -10,38 +10,12 @@ use embedded_graphics::mono_font::ascii::FONT_6X10;
 use embedded_graphics::mono_font::MonoTextStyleBuilder;
 use embedded_graphics::text::Baseline;
 use embedded_graphics_simulator::{BinaryColorTheme, SimulatorDisplay, Window, OutputSettingsBuilder};
-
-#[derive(Clone, Default)]
-pub struct DisplayState {
-    time: NaiveTime,
-    lat: f64,
-    lon: f64,
-    sats: u8,
-}
-
+use traccam_common::display::DisplayState;
 
 fn main() -> Result<(), core::convert::Infallible> {
     let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(128, 32));
 
-    let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_6X10)
-        .text_color(BinaryColor::On)
-        .build();
-    let state = DisplayState::default();
-
-    display.clear(BinaryColor::Off).unwrap();
-
-    Text::with_baseline(&heapless::format!(15; "{} {}", state.time, state.sats).unwrap(), Point::zero(), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
-
-    Text::with_baseline(&heapless::format!(10; "N{:.5}", state.lat).unwrap(), Point::new(0, 9), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
-
-    Text::with_baseline(&heapless::format!(10; "E{:.5}", state.lon).unwrap(), Point::new(0, 18), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+    traccam_common::display::draw_status_display(&mut display, &DisplayState::default());
 
     let output_settings = OutputSettingsBuilder::new()
         .theme(BinaryColorTheme::OledWhite)
